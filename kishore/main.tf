4846196a792d9eae0f62a9309994fe974c9a46e2
anadu <kishreddy09@gmail.com>
2021-12-16 19:59:55 +0530

    Update main.tf

--- kishore/main.tf
+++ kishore/main.tf
@@ -1,5 +1,5 @@
 # Create EC2 Instance - Amazon2 Linux
-resource "aws_instance" "my-ec2-vm" {
+resource "aws_instance" "my-ec2-vm1" {
   ami           = data.aws_ami.amzlinux.id 
   instance_type = var.instance_type  
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
